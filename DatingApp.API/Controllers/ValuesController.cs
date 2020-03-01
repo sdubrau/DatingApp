7cc286b305279e6eac61d7ec9655c915b56a20fa
s.dubrau <s.dubrau@technologyandstrategy.com>
2020-03-01 11:38:29 +0100

    Module2

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -4,32 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using DatingApp.API.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/api/[controller]")]
     public class ValuesController : ControllerBase
     {
-      
+        private readonly DataContext _context;
+      public ValuesController(DataContext context)
+      {
+          _context=context;
+      }
         private readonly ILogger<ValuesController> _logger;
 
-        public ValuesController(ILogger<ValuesController> logger)
-        {
-            _logger = logger;
-        }
 
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<IActionResult> GetValues()
         {
-            return new string[] {"value1","value2"};
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
         }
 
           [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            
-            return "value";
+            var value = await _context.Values.FirstOrDefaultAsync(e=>e.Id==id);
+            return Ok(value);
         }
 
           [HttpPut]
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -11,6 +11,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using DatingApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace DatingApp.API
 {
     public class Startup
@@ -25,8 +28,10 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
-            services.AddCors()
+            services.AddCors();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +45,7 @@ namespace DatingApp.API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
-            app.UseCors();
+            app.UseCors(x=>x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
