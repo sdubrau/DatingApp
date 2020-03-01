510e06ba0c480ebe658ea5e3be9bc06740198d5a
s.dubrau <s.dubrau@technologyandstrategy.com>
2020-03-01 17:32:55 +0100

    Auth

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -14,6 +14,6 @@ values: any;
 this.getValues();
   }
 getValues() {
-  this.http.get('http://localhost:5000/api/values').subscribe(response => { this.values = response; }, error => {console.log(error);});
+  this.http.get('http://localhost:5000/api/values').subscribe(response => { this.values = response; }, error => { console.log(error); });
 }
 }
--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore;
 namespace DatingApp.API.Controllers
 {
     [ApiController]
-    [Route("/api/[controller]")]
+    [Route("api/[controller]")]
     public class ValuesController : ControllerBase
     {
         private readonly DataContext _context;
